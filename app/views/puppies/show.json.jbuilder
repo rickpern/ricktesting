7f7c3692d922cc76e6b570774a0415e20b73d9b8
Richard Pernikoff <rickpern@gmail.com>
2015-09-07 17:32:58 -0500

    add puppies scaffolding

---
+++ app/views/puppies/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @puppy, :id, :name, :breed, :cuteness, :created_at, :updated_at
---
+++ db/migrate/20150907223108_create_puppies.rb
@@ -0,0 +1,11 @@
+class CreatePuppies < ActiveRecord::Migration
+  def change
+    create_table :puppies do |t|
+      t.string :name
+      t.string :breed
+      t.integer :cuteness
+
+      t.timestamps null: false
+    end
+  end
+end
